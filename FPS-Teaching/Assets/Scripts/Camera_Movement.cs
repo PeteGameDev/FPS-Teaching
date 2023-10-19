82bb84ce0ddfc8f772fba374d1bdb371fd0b5357
GameDevPete <99825504+PeteGameDev@users.noreply.github.com>
2023-10-19 08:52:15 +0100

    basic movement

---
+++ FPS-Teaching/Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{   
+    public Camera cam;
+    public float lookSpeed;
+    public float maximumX = 60f;
+    public float minimumX = -60f; //these two lines are for locking camera rotation
+
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMove();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void CameraMove(){
+        //get inputs
+        rotationX += Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationY += Input.GetAxis("Mouse X") * lookSpeed;
+
+        //clamp rotation
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+        //apply rotation
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotationX, 0, 0);
+    }
+}
--- FPS-Teaching/Assets/Scripts/Player_Movement.cs
+++ FPS-Teaching/Assets/Scripts/Player_Movement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+    public float jumpHeight;
+    private float gravity = -9.81f;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+    
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        if(Input.GetKeyDown("space") && controller.isGrounded){
+            Jump();
+        }
+    }
+
+    void Move(){
+        //get inputs
+        float moveHorizontal = Input.GetAxisRaw("Horizontal"); //this will return input value of -1, 0, 1
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        //do maths
+        moveDirection = (moveHorizontal * transform.right + moveVertical * transform.forward);
+
+        //apply maths
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        //apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Jump(){
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); 
     }
 }
