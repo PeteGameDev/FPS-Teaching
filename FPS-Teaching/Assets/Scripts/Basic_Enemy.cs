8232b601d3f9463e6c5aae601dcd8cddcbc972d4
GameDevPete <99825504+PeteGameDev@users.noreply.github.com>
2023-11-08 12:41:56 +0000

    Added basic enemy

---
+++ FPS-Teaching/Assets/Scripts/Basic_Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Basic_Enemy : MonoBehaviour
+{   
+    public float damageAmount, attackDelay, attackRate, attackDistance;
+    GameObject playerObject;
+    NavMeshAgent navAgent;
+
+    void Start()
+    {
+        playerObject = GameObject.FindWithTag("Player");
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Time.time > attackDelay){
+            Move();
+            if(Vector3.Distance(playerObject.transform.position, transform.position) <= attackDistance){
+                Attack();
+            }
+        }
+        else navAgent.destination = transform.position;
+    }
+
+    void Attack(){
+        //Melee Attack
+        playerObject.GetComponent<Player_Health>().playerHealth -= damageAmount;
+        attackDelay = Time.time + attackRate;
+    }
+
+    void Move(){
+        navAgent.destination = playerObject.transform.position;
+    }
+}
---
+++ FPS-Teaching/Assets/Scripts/Player_Health.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour
+{
+    public float playerHealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth <= 0){
+            GameOver();
+        }
+    }
+
+    void GameOver(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
